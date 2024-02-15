13cca67a92cbb14020a55a8c9bd9ac4275c9f740
Johnnyboydensing <riverabrian@gmail.com>
2024-02-15 20:59:31 +0800

    SEcond

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", crudRoute);
 app.use(
   cors({
-    origin: "http://192.168.1.17:8080/",
+    origin: ["http://192.168.1.17:8080/", "http://127.0.0.1:8080"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
--- client/index.js
+++ client/index.js
@@ -1,12 +1,13 @@
 /** @format */
 const getAx = async () => {
-  try {
-    const res = await axios.get(`https://crud-blgw.onrender.com/api`);
-    console.log("Ok");
-    return res;
-  } catch (error) {
-    console.log(error);
-  }
+  // try {
+  //   const res = await axios.get(`https://crud-blgw.onrender.com/api`);
+  //   console.log("Ok");
+  //   return res;
+  // } catch (error) {
+  //   console.log(error);
+  // }
+  console.log("OK");
 };
 
 getAx();
